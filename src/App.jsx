5dc2ed70e8cbe4f9856cca5338efe95f2fdd794b
casprine <casprine.001@gmail.com>
2023-06-18 20:50:11 +0200

    richtext support

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,152 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { getData, setData } from './lib/storage.js';
+import { nanoid } from 'nanoid';
+import { format } from 'date-fns';
 
 import { Editor } from './components/Editor.jsx';
 
 import './App.css';
+import './toolbar.css';
+import './fonts.css';
 
-const EmptyState = () => {
+const CONTENT_KEY = 'opps-content';
+
+const EmptyState = ({ addNewEntry }) => {
   return (
     <div className="full-center">
-      <h4>Hello Cas</h4>
+      <h4>Hello</h4>
       <p>Welcome to your new petty nightmare</p>
-      <button>Add new entry</button>
+      <button onClick={addNewEntry} className="shadow-button">
+        Add new entry
+      </button>
+    </div>
+  );
+};
+
+const ContentListItem = ({ title, createdAt, onClick, onDelete }) => {
+  return (
+    <div className="contentlist-item" onClick={onClick}>
+      <p className="title">{title}</p>
+      <div className="createdAt">{format(new Date(createdAt), 'd MMM, yyyy - p')}</div>
+      <button
+        className="delete-btn shadow-button"
+        onClick={(e) => {
+          e.stopPropagation();
+          onDelete();
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 };
 
 function App() {
   const [content, setContent] = useState([]);
+  const [currentContentState, setCurrentContentState] = useState(null);
 
   useEffect(() => {
-    const data = getData('content') ?? [];
-
+    const data = getData(CONTENT_KEY) ?? [];
     setContent(data);
   }, []);
 
-  // if (content?.length <= 0) {
-  //   return (
-  //     <div className="empty-state-container">
-  //       <EmptyState />
-  //     </div>
-  //   );
-  // }
+  const saveEditedContent = (editorState) => {
+    const firstTextChild = editorState?.root?.children?.[0]?.children?.[0]?.text ?? new Date().toISOString();
 
-  return (
-    <div className="container">
-      <Editor />
-    </div>
-  );
+    const entry = {
+      ...currentContentState,
+      updatedAt: new Date().toISOString(),
+      content: JSON.stringify(editorState),
+      title: firstTextChild,
+    };
+
+    const updatedContent = [...content.filter((item) => item.id !== currentContentState.id), entry];
+
+    setData(CONTENT_KEY, updatedContent);
+    setContent(updatedContent);
+  };
+
+  const saveContent = (editorState) => {
+    // designated as an edit if currentContentState.id exist
+    if (currentContentState.id) {
+      saveEditedContent(editorState);
+      return;
+    }
+
+    const firstTextChild = editorState?.root?.children?.[0]?.children?.[0]?.text ?? new Date().toISOString();
+    const entry = {
+      id: nanoid(),
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      content: JSON.stringify(editorState),
+      title: firstTextChild,
+    };
+
+    const updatedContent = [...content, entry];
+
+    setData(CONTENT_KEY, updatedContent);
+    setContent(updatedContent);
+  };
+
+  const deleteListing = (id) => {
+    const updatedContent = content.filter((item) => item?.id !== id);
+    setData(CONTENT_KEY, updatedContent);
+    setContent(updatedContent);
+  };
+
+  const addNewEntry = () => {
+    setCurrentContentState({
+      content: '',
+      createdAt: new Date().toISOString(),
+    });
+  };
+
+  if (currentContentState) {
+    return (
+      <div className="container">
+        <Editor
+          onBackClick={() => setCurrentContentState(null)}
+          saveContent={saveContent}
+          lastEditDate={currentContentState?.updatedAt ?? currentContentState?.createdAt}
+          content={currentContentState?.content ? JSON.parse(currentContentState.content) : null}
+        />
+      </div>
+    );
+  }
+
+  if (content?.length <= 0) {
+    return (
+      <div className="empty-state-container">
+        <EmptyState addNewEntry={addNewEntry} />
+      </div>
+    );
+  }
+
+  if (content.length > 0) {
+    return (
+      <div className="container contentlist-container">
+        <div className="header">
+          <h2>All list</h2>
+          <button className="shadow-button" onClick={addNewEntry}>
+            New Entry
+          </button>
+        </div>
+        {content.map((content, index) => {
+          return (
+            <Fragment key={content.id}>
+              <ContentListItem
+                onClick={() => setCurrentContentState(content)}
+                title={content.title}
+                createdAt={content?.createdAt}
+                onDelete={() => deleteListing(content.id)}
+              />
+              {content.length - 1 !== index && <div className="hr-divider" />}
+            </Fragment>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,27 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
-import { $getRoot, $getSelection } from 'lexical';
+import { useEffect, useRef } from 'react';
+import { formatDistance } from 'date-fns';
 
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import { TableCellNode, TableNode, TableRowNode } from '@lexical/table';
+import { ListItemNode, ListNode } from '@lexical/list';
+import { CodeHighlightNode, CodeNode } from '@lexical/code';
+import { AutoLinkNode, LinkNode } from '@lexical/link';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
+import { ListPlugin } from '@lexical/react/LexicalListPlugin';
+import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
+import { TRANSFORMERS } from '@lexical/markdown';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 
-const theme = {
-  // Theme styling goes here
-};
-
-function onChange(editorState) {
-  editorState.read(() => {
-    // Read the contents of the EditorState here.
-    const root = $getRoot();
-    const selection = $getSelection();
-
-    console.log(root, selection);
-  });
-}
+// import AutoLinkPlugin from '../plugins/AutolinkPlugin';
+import ToolbarPlugin from '../plugins/ToolbarPlugin';
+import { theme } from '../plugins/theme';
 
 function MyCustomAutoFocusPlugin() {
   const [editor] = useLexicalComposerContext();
@@ -38,33 +37,63 @@ function onError(error) {
   console.error(error);
 }
 
-export const Editor = () => {
+export const Editor = ({ saveContent, content, onBackClick, lastEditDate }) => {
   const editorState = useRef();
 
   const initialConfig = {
     namespace: 'ContentEditor',
     theme,
     onError,
+    editorState: content ? JSON.stringify(content) : null,
+
+    nodes: [
+      HeadingNode,
+      ListNode,
+      ListItemNode,
+      QuoteNode,
+      CodeNode,
+      CodeHighlightNode,
+      TableNode,
+      TableCellNode,
+      TableRowNode,
+      AutoLinkNode,
+      LinkNode,
+    ],
   };
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
+      <RichTextPlugin
         contentEditable={
           <div className="editor-container">
+            <p className="last-edited">
+              Last edited at: <span>{formatDistance(new Date(lastEditDate), new Date())}</span>
+            </p>
+            <ToolbarPlugin />
             <ContentEditable className="editor-input" />
+            <div className="save-button">
+              <button className="back-btn shadow-button" onClick={onBackClick}>
+                Go Back
+              </button>
+              <button className="shadow-button" onClick={() => saveContent(editorState.current.toJSON())}>
+                Save Changes
+              </button>
+            </div>
           </div>
         }
         ErrorBoundary={LexicalErrorBoundary}
       />
       <OnChangePlugin
         onChange={(state) => {
-          onChange(state);
           editorState.current = state;
         }}
       />
+
+      <ListPlugin />
+      <LinkPlugin />
       <HistoryPlugin />
       <MyCustomAutoFocusPlugin />
+      {/* <AutoLinkPlugin /> */}
     </LexicalComposer>
   );
 };
