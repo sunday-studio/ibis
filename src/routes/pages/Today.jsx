f10aeba8de9ade0911e925f9a3a8fdd87126d50f
casprine <casprine.001@gmail.com>
2023-07-23 15:20:07 +0300

    setup today page layout

--- src/routes/pages/Today.jsx
+++ src/routes/pages/Today.jsx
@@ -1,7 +1,11 @@
 export default function TodayPage() {
   return (
-    <div className="container">
-      <p> Today page </p>
+    <div className="container today-page">
+      <div className="notes">Notes</div>
+      <div className="sider">
+        <div className="todos">Todos</div>
+        <div className="calendar">Calendar</div>
+      </div>
     </div>
   );
 }
