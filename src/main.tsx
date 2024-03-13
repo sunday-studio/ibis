fa3183784221b042a62564ff59318a33f639e2d9
casprine <casprine.001@gmail.com>
2024-03-13 12:03:50 +0100

    add sentry for error tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,29 @@
-// @ts-nocheck
 import React from 'react';
 
+import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes/router';
 import './styles/index.scss';
+
+Sentry.init({
+  dsn: 'https://0b362d94f01e0a41d9fba0854d04f657@o4506903320068096.ingest.us.sentry.io/4506903329243136',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
