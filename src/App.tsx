a11722d2b8027b03ffd3e0303925ccc9ac06f611
casprine <casprine.001@gmail.com>
2025-02-02 11:29:22 +0100

    remove node modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,10 @@
-import {useState} from "react";
 import reactLogo from "./assets/react.svg";
-import {invoke} from "@tauri-apps/api/core";
 import "./App.css";
 import Database from "@tauri-apps/plugin-sql";
 
 const db = await Database.load("sqlite:ibis.db");
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  // console.log(db);
-
-  const entry = {
-    id: 1,
-    title: "Learn Tauri",
-    content: "Learn Tauri",
-    isPinned: false,
-    isDuplicate: false,
-    tagsId: null,
-  };
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    // setGreetMsg(await invoke("greet", {name}));
-    // const result = await db.execute(
-    //   "INSERT INTO entries (title, content, isPinned, isDuplicate, createdAt, updatedAt, tagsId) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-    //   [
-    //     entry.title,
-    //     entry.content,
-    //     entry.isPinned ?? false, // Default to false if not provided
-    //     entry.isDuplicate ?? false, // Default to false if not provided
-    //     new Date().toISOString(), // Set createdAt timestamp
-    //     new Date().toISOString(), // Set updatedAt timestamp
-    //     entry.tagsId || null, // Allow null if no tags
-    //   ]
-    // );
-    // console.log("result ===>", result);
-  }
-
   async function getEntries() {
     const result = await db.select("SELECT * FROM entries");
     console.log("result ===>", result);
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  plugins: [react(), tailwindcss()],
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -25,7 +20,6 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
